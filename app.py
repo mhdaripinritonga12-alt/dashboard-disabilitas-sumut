d9129cf96971cc2aa01e16ece41628bb9f738d4e
mhdaripinritonga12-alt <mhdaripinritonga12@gmail.com>
2026-04-11 17:24:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,13 @@ st.markdown("""
     .tile-blue-light { background: linear-gradient(135deg, #03a9f4 0%, #0288d1 100%); }
     .tile-red-dark { background: linear-gradient(135deg, #ff4b2b 0%, #ff416c 100%); }
     .tile-green-light { background: linear-gradient(135deg, #98ec2d 0%, #17ad4a 100%); }
-    .tile-icon-svg { width: 40px; height: 40px; fill: white; }
+    .tile-icon-svg { 
+    width: 42px;
+    height: 42px;
+    display: flex;
+    align-items: center;
+    margin-top: 4px; /* ini bikin sedikit turun */
+}
     .tile-label { font-size: 11px; font-weight: 600; text-transform: uppercase; }
     .tile-value { font-size: 22px; font-weight: 800; }
 
@@ -86,7 +92,7 @@ svg_people = '<svg viewBox="0 0 16 16"><path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1
 svg_cap = '<svg viewBox="0 0 16 16"><path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3 7.5-3a.5.5 0 0 0 .025-.917l-7.5-3.5Z"/><path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .254.539l1.5.75A.5.5 0 0 0 5.25 12h5.5a.5.5 0 0 0 .476-.346l.5-1.7a.5.5 0 0 0-.656-.327L10 10.25l-.117-.043-4 .876L4.176 9.032Z"/></svg>'
 svg_warning = '<svg viewBox="0 0 16 16"><path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/></svg>'
 svg_chart = '''
-<svg viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+<svg viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round">
   <path d="M4 14 L9 9 L13 13 L20 6"></path>
 </svg>
 '''
