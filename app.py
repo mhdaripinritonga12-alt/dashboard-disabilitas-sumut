43343bd00ea72a8c5985ec4096516198841f06a2
mhdaripinritonga12-alt <mhdaripinritonga12@gmail.com>
2026-04-12 15:32:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -333,10 +333,6 @@ if st.session_state.page_view == "dashboard":
                 p_insight, p_tindakan, warna_box = f"💡 Wilayah {kab_pilih} memiliki {v_a:,} ATS dengan partisipasi {v_aps}.", "Optimalkan sekolah terdekat.", "#0d47a1"
 
            st.markdown(f'<div class="insight-box" style="border-left: 6px solid {warna_box};"><div class="insight-title" style="color:{warna_box}">💡 Insight & Rekomendasi: {kab_pilih}</div><p class="insight-text">{p_insight}</p><hr style="opacity:0.3;"><p class="insight-text" style="font-weight:700; color:{warna_box}">Tindakan: {p_tindakan}</p></div>', unsafe_allow_html=True)
-
-            # =========================
-            # 🔥 MASALAH UTAMA (FIX)
-            # =========================
             if v_a > 0:
                 teks_masalah = f"Masih tingginya jumlah Anak Tidak Sekolah (ATS) Disabilitas di wilayah {kab_pilih} sebanyak {v_a:,} jiwa."
             else:
